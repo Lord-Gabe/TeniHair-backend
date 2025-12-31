880b9bafe7905cfd737e9bce7e6d484e13626a4c
Lord-Gabe <gabolusesan@gmail.com>
2025-12-31 16:35:58 +0100

    fourth commit

--- routes/contact.js
+++ routes/contact.js
@@ -20,7 +20,7 @@ router.post("/", async (req, res) => {
 
     const transporter = nodemailer.createTransport({
       host: process.env.MAIL_HOST,
-      port: process.env.MAIL_PORT,
+      port: Number(process.env.MAIL_PORT),
       secure: false,
       auth: {
         user: process.env.MAIL_USER,
