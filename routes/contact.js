1575918e245aa3c11abd3bea205cb66d8e66575a
Lord-Gabe <gabolusesan@gmail.com>
2025-12-31 15:56:06 +0100

    second commit

--- routes/contact.js
+++ routes/contact.js
@@ -19,7 +19,9 @@ router.post("/", async (req, res) => {
 
 
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: process.env.MAIL_HOST,
+      port: process.env.MAIL_PORT,
+      secure: false,
       auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
