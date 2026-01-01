43d310948d40826432d160b6bc8f18513a517e96
Lord-Gabe <gabolusesan@gmail.com>
2026-01-01 12:02:48 +0100

    8th change

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 import express from "express";
 import contactRouter from "./routes/contact.js"; // adjust path
+import cors from "cors";
 
 const app = express();
 
+
+// CORS (if your frontend is on a different domain)
+
+app.use(
+  cors({
+    origin: "https://teni-hair-frontend.vercel.app/", // e.g. "https://your-frontend.com"
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+  })
+);
+
 // Parse JSON bodies
 app.use(express.json());
-
-// // CORS (if your frontend is on a different domain)
-// import cors from "cors";
-// app.use(
-//   cors({
-//     origin: process.env.CLIENT_ORIGIN || "*", // e.g. "https://your-frontend.com"
-//   })
-// );
 
 // Mount the contact route
 app.use("/api/contact", contactRouter);
